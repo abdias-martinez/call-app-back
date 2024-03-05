974535ff20f3646ce20d508354fe5b67d0c905a0
Ever Avendaño <ever.avendano@unmsm.edu.pe>
2024-03-05 12:00:25 -0500

    Add comments

--- app.py
+++ app.py
@@ -6,133 +6,178 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
-    
+
+# ruta para el proceso de inicio de sesión
 @app.route("/login", methods=["POST"])
 def login():
     try:
+        # conexión a la base de datos
         connection = mysql.connector.connect(**DB_CONFIG)
         print("Conexion exitosa")
+
         try:
+            # obtenemos datos de la solicitud JSON
             data = request.get_json()
             username = data.get("username")
             password = data.get("password")
 
             print(f"username: {username}, password: {password}")
+
+            # Validamos el nombre de usuario y contraseña
             if not username or not password:
                 return jsonify({"error": "Usuario y contraseña son requeridos"}), 400
             else:
+                # generamos un token
                 token = auth_user(username, connection, password, SECRET_KEY_CONFIG['token_secret_key'])
                 if token:
+                    print("Autenticacion exitosa")
                     return jsonify({"token": token})
                 else:
+                    print("Autenticacion fallida")
                     return jsonify({"error": "Autenticacion fallida"}), 401
 
         except Exception as e:
             print(f"Error: {e}")
             return jsonify({"error": "Error interno del servidor"}), 500
-    
-    except mysql.connector.Error:
-        return jsonify({"type":"error", "message": "Error en la conexión"})
-        
+
+    except mysql.connector.Error as err:
+        print(f"Error de conexion: {err}")
+        return jsonify({"type": "error", "message": "Error en la conexión"})
+
+# Ruta para insertar registros en una tabla
 @app.route("/dashboard/<view>/<token>", methods=["POST"])
 def insertRecordTable(view, token):
     try:
+        # conexión a la base de datos
         connection = mysql.connector.connect(**DB_CONFIG)
         print("Conexion exitosa")
+
+        # verificamos si el usuario cuenta con autenticación
         if token != "" and user_is_authenticated(token, connection, SECRET_KEY_CONFIG['token_secret_key']):
             try:
                 response = request.get_json()
                 record = response["insertRecord"]
+                # insertamos el registro en la tabla
                 sendData = insert_records_table(token, record, connection, view, SECRET_KEY_CONFIG['token_secret_key'])
                 return jsonify(sendData)
-            except Exception:
-                return jsonify({"type":"error", "message": "Error interno del servidor"})
+            except Exception as e:
+                print(f"Error: {e}")
+                return jsonify({"type": "error", "message": "Error interno del servidor"})
         else:
+            print("No se encuentra autenticado")
             return jsonify({"type": "error", "message": "No se encuentra autenticado"})
-    except mysql.connector.Error:
-        return jsonify({"type":"error", "message": "Falló la conexión con la base de datos"})
 
+    except mysql.connector.Error as err:
+        print(f"Error de conexion: {err}")
+        return jsonify({"type": "error", "message": "Falló la conexión con la base de datos"})
+
+# Ruta para obtener registros de una tabla específica
 @app.route("/dashboard/<view>/<token>", methods=["GET"])
 def obtainRecordsTable(view, token):
     try:
+        # conexión a la base de datos
         connection = mysql.connector.connect(**DB_CONFIG)
-        
+
         print(f"token: {token}")
-        
+
+        # verificamos si el usuario cuenta con autenticación
         print(f"result: {user_is_authenticated(token, connection, SECRET_KEY_CONFIG['token_secret_key'])}")
-        
-        if token != "" and user_is_authenticated(token, connection, SECRET_KEY_CONFIG['token_secret_key']): 
+        if token != "" and user_is_authenticated(token, connection, SECRET_KEY_CONFIG['token_secret_key']):
             try:
+                # obtenemos los registros de la tabla
                 data = get_records_table(token, connection, view, SECRET_KEY_CONFIG['token_secret_key'])
                 print(f"data: {data}")
                 return jsonify(data)
 
-            except Exception:
+            except Exception as e:
+                print(f"Error: {e}")
                 return jsonify({"error": "Error interno del servidor"}), 500
         else:
+            print("No se encuentra autenticado")
             return jsonify({"type": "error", "message": "No se encuentra autenticado"})
-        
-    except mysql.connector.Error:
-        return jsonify({"type":"error", "message": "Error en la conexión"})
-        
+
+    except mysql.connector.Error as err:
+        print(f"Error de conexion: {err}")
+        return jsonify({"type": "error", "message": "Error en la conexión"})
+
+# Ruta para obtener todos los registros de todas las tablas
 @app.route("/dashboard/home/<token>", methods=["GET"])
 def obtainAllTablesRecords(token):
     try:
+        # conexión a la base de datos
         connection = mysql.connector.connect(**DB_CONFIG)
-        if token != "" and user_is_authenticated(token, connection, SECRET_KEY_CONFIG['token_secret_key']): 
+        if token != "" and user_is_authenticated(token, connection, SECRET_KEY_CONFIG['token_secret_key']):
             try:
+                # Obtenemos los registros de todas las tablas
                 data = get_all_tables_records(token, connection, SECRET_KEY_CONFIG['token_secret_key'])
                 return jsonify(data)
 
-            except Exception:
+            except Exception as e:
+                print(f"Error: {e}")
                 return jsonify({"error": "Error interno del servidor"}), 500 
         else:
+            print("No se encuentra autenticado")
             return jsonify({"type": "error", "message": "No se encuentra autenticado"})
-    except mysql.connector.Error:
-        return jsonify({"type":"error", "message": "Error en la conexión"})
+    except mysql.connector.Error as err:
+        print(f"Error de conexion: {err}")
+        return jsonify({"type": "error", "message": "Error en la conexión"})
 
+# Ruta para actualizar un registro en una tabla
 @app.route("/dashboard/<view>/<token>", methods=["PUT"])
 def updateRecordTable(view, token):
     try:
+        # conexión a la base de datos
         connection = mysql.connector.connect(**DB_CONFIG)
         print("Conexion exitosa")
-        
-        if token != "" and user_is_authenticated(token, connection, SECRET_KEY_CONFIG['token_secret_key']): 
+
+        # verificamos si el usuario cuenta con autenticación
+        if token != "" and user_is_authenticated(token, connection, SECRET_KEY_CONFIG['token_secret_key']):
             try:
                 response = request.get_json()
                 identifier = response["identifier"]
                 editedRecord = response["editedRecord"]
                 originalRecord = response["originalRecord"]
+                # Actualizar registro en la tabla por identificador
                 sendData = update_record_table_by_id(token, editedRecord, originalRecord, connection, identifier, view, SECRET_KEY_CONFIG['token_secret_key'])
                 return jsonify(sendData)
-            except Exception:
-                return jsonify({"type":"error", "message": "Error en el servidor"})
-        else: 
+            except Exception as e:
+                print(f"Error: {e}")
+                return jsonify({"type": "error", "message": "Error en el servidor"})
+        else:
+            print("No se encuentra autenticado")
             return jsonify({"type": "error", "message": "No se encuentra autenticado"})
-        
-    except mysql.connector.Error:
-        return jsonify({"type":"error", "message": "Error en la conexión"})
-    
+
+    except mysql.connector.Error as err:
+        print(f"Error de conexion: {err}")
+        return jsonify({"type": "error", "message": "Error en la conexión"})
+
+# Ruta para eliminar un registro de una tabla
 @app.route("/dashboard/<view>/<token>", methods=["DELETE"])
 def deleteRecordTable(view, token):
     try:
+        # conexión a la base de datos
         connection = mysql.connector.connect(**DB_CONFIG)
         print("Conexion exitosa")
-        
+
+        # verificamos si el usuario cuenta con autenticación
         if token != "" and user_is_authenticated(token, connection, SECRET_KEY_CONFIG['token_secret_key']):
             try:
                 response = request.get_json()
                 record_id = response["recordIdentifier"]
                 identifier = response["identifier"]
+                # eliminamos cierto registro de la tabla
                 sendData = delete_record_table_by_id(token, record_id, connection, identifier, view, SECRET_KEY_CONFIG['token_secret_key'])
                 return jsonify(sendData)
-            except Exception:
-                return jsonify({"type":"error", "message": "Error en el servidor"})
+            except Exception as e:
+                print(f"Error: {e}")
+                return jsonify({"type": "error", "message": "Error en el servidor"})
         else:
+            print("No se encuentra autenticado")
             return jsonify({"type": "error", "message": "No se encuentra autenticado"})
-        
-    except mysql.connector.Error:
-        return jsonify({"type":"error", "message": "Error en la conexión"})
-        
+
+    except mysql.connector.Error as err:
+        print(f"Error de conexion: {err}")
+        return jsonify({"type": "error", "message": "Error en la conexión"})
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3005, debug=True)
--- utils.py
+++ utils.py
@@ -3,66 +3,83 @@ import json
 import jwt
 from datetime import datetime
 from config import TB_NAMES
-    
+
+# función para formatear la fecha y hora
 def format_datetime(value):
     if isinstance(value, datetime):
         return value.strftime('%Y-%m-%d %H:%M:%S')
     else:
         return value
 
+# función para insertar registros en una tabla
 def insert_records_table(token: str, data_dict: dict, connection: mysql.connector.connection.MySQLConnection, view: str, secret_key: str) -> dict:
     decoded_token = jwt.decode(token, secret_key, algorithms='HS256')
     user_rol = decoded_token["user_rol"]
+
     if view in TB_NAMES.keys():
         table_name = TB_NAMES[view]['name']
+
+        # verificamos los permisos del usuario para acceder a la vista
         if user_rol in TB_NAMES[view]['accessed_by']:
             try:
                 cursor = connection.cursor()
+
+                # verificamos si el usuario ya existe en la tabla 'Usuarios'
                 if table_name == 'Usuarios':
                     existing_user_query = f"SELECT COUNT(*) FROM {table_name} WHERE Usuario = %s"
                     cursor.execute(existing_user_query, (data_dict['Usuario'],))
                     user_count = cursor.fetchone()[0]
                     if user_count > 0:
                         return {'type': 'error', 'message': 'Usuario ya existe'}
+
                 columns = ', '.join(data_dict.keys())
                 placeholders = ', '.join(['%s'] * len(data_dict))
                 sql_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
                 values = tuple(data_dict.values())
+
                 cursor.execute(sql_query, values)
                 connection.commit()
+
                 return {'type': 'success', 'message': 'Registro insertado'}
+
             except mysql.connector.Error as err:
                 return {'type': 'error', 'message': 'Error en la inserción'}
+
             finally:
                 cursor.close()
         else: 
             return {"type": "error", "message": "No cuenta con permisos para realizar esta acción"}
     else:
         return {"type": "error", "message": "Vista de origen no existe"}
-        
-        
+
+# función para obtener registros de una tabla
 def get_records_table(token: str, connection: mysql.connector.connection.MySQLConnection, view: str, secret_key: str) -> str:
     decoded_token = jwt.decode(token, secret_key, algorithms='HS256')
     user_rol = decoded_token["user_rol"]
+
     if view in TB_NAMES.keys():
         table_name = TB_NAMES[view]['name']
+
+        # verificamos los permisos del usuario para acceder a la vista
         if user_rol in TB_NAMES[view]['accessed_by']:
             try:
                 cursor = connection.cursor(dictionary=True)
 
+                # Obtenemos los nombres de las columnas de la tabla
                 cursor.execute(f"SHOW COLUMNS FROM {table_name}")
-
                 columns = [column['Field'] for column in cursor.fetchall()]
                 columns_except_id = [column for column in columns if column.lower() != 'id']
                 columns_str = ', '.join(columns_except_id)
+
+                # ejecutamos la consulta SQL para obtener registros
                 sql_query = f"SELECT {columns_str} FROM {table_name}"
                 cursor.execute(sql_query)
                 records = cursor.fetchall()
-                formatted_records = []
-                for record in records:
-                    formatted_record = {key: format_datetime(value) for key, value in record.items()}
-                    formatted_records.append(formatted_record)
+
+                # formateamos los registros antes de convertirlos a JSON
+                formatted_records = [{key: format_datetime(value) for key, value in record.items()} for record in records]
                 json_result = json.dumps(formatted_records, indent=2)
+
                 return json_result
 
             except mysql.connector.Error as err:
@@ -75,28 +92,22 @@ def get_records_table(token: str, connection: mysql.connector.connection.MySQLCo
             return {"type": "error", "message": "No cuenta con permisos para realizar esta acción"}
     else:
         return {"type": "error", "message": "Vista de origen no existe"}
-        
-                
+
+# función para obtener registros de todas las tablas
 def get_all_tables_records(token: str, connection: mysql.connector.connection.MySQLConnection, secret_key: str) -> str:
     try:
         cursor = connection.cursor(dictionary=True)
         cursor.execute("SHOW TABLES")
-        
+
         decoded_token = jwt.decode(token, secret_key, algorithms='HS256')
-        print(f"decode_token: {decoded_token}")
         user_rol = decoded_token["user_rol"]
-        
+
         tables = [table['Tables_in_CAE'] for table in cursor.fetchall()]
-        
-        print(f"tables: {tables}")
-        
-        print(f"tb_names_keys: {TB_NAMES.keys()}")
-        
+
+        # eliminamos las tablas para las que el usuario no tiene permisos
         for tb in TB_NAMES.values():
             if user_rol not in tb['accessed_by']:
                 tables.remove(tb['name'])
-            
-        print(f"tables: {tables}")
 
         all_tables_data = {}
 
@@ -106,8 +117,6 @@ def get_all_tables_records(token: str, connection: mysql.connector.connection.My
             columns = [column['Field'] for column in columns_info]
 
             columns_except_id = [column for column in columns if column.lower() != 'id']
-            #columns_except_id = [column for column in columns]
-
             columns_str = ', '.join(columns_except_id)
 
             sql_query = f"SELECT {columns_str} FROM {table_name}"
@@ -115,10 +124,8 @@ def get_all_tables_records(token: str, connection: mysql.connector.connection.My
 
             records = cursor.fetchall()
 
-            formatted_records = []
-            for record in records:
-                formatted_record = {key: format_datetime(value) for key, value in record.items()}
-                formatted_records.append(formatted_record)
+            # formateamos los registros antes de agregarlos al resultado final
+            formatted_records = [{key: format_datetime(value) for key, value in record.items()} for record in records]
 
             table_data = {
                 "table_name": table_name,
@@ -137,25 +144,31 @@ def get_all_tables_records(token: str, connection: mysql.connector.connection.My
 
     finally:
         cursor.close()
-        
 
+# función para actualizar un registro en una tabla por identificador
 def update_record_table_by_id(token: str, new_data_dict: dict, original_data_dict: dict, connection: mysql.connector.connection.MySQLConnection, identifier: str, view: str, secret_key: str) -> dict:
     decoded_token = jwt.decode(token, secret_key, algorithms='HS256')
     user_rol = decoded_token["user_rol"]
+
     if view in TB_NAMES.keys():
         table_name = TB_NAMES[view]['name']
+
+        # verificamos los permisos del usuario para acceder a la vista
         if user_rol in TB_NAMES[view]["accessed_by"]:
             try:
                 cursor = connection.cursor()
-                
+
+                # construimos la cláusula SET para la actualización
                 set_clause = ', '.join([f"{key} = %s" for key in new_data_dict.keys()])
                 where_condition = f"{identifier} = %s"
-                
+
+                # construimos y ejecutamos la consulta SQL
                 sql_query = f"UPDATE {table_name} SET {set_clause} WHERE {where_condition}"
                 values = tuple([new_data_dict[key] for key in new_data_dict.keys()] + [original_data_dict[identifier]])
-                
+
                 cursor.execute(sql_query, values)
                 connection.commit()
+
                 return {'type': 'success', 'message': 'Registro editado'}
 
             except mysql.connector.Error as err:
@@ -169,25 +182,26 @@ def update_record_table_by_id(token: str, new_data_dict: dict, original_data_dic
     else:
         return {"type": "error", "message": "Vista de origen no existe"}
 
-
-def delete_record_table_by_id(token: str, id: str, connection: mysql.connector.connection.MySQLConnection, identifier: str, view: str, secret_key: str) -> None:
+# función para mos las un registro de una tabla por identificador
+def delete_record_table_by_id(token: str, id: str, connection: mysql.connector.connection.MySQLConnection, identifier: str, view: str, secret_key: str) -> dict:
     decoded_token = jwt.decode(token, secret_key, algorithms='HS256')
     user_rol = decoded_token["user_rol"]
+
     if view in TB_NAMES.keys():
         table_name = TB_NAMES[view]['name']
+
+        # verificamos los permisos del usuario para acceder a la vista
         if user_rol in TB_NAMES[view]["accessed_by"]:
             try:
                 cursor = connection.cursor()
 
                 where_condition = f"{identifier} = %s"
 
+                # construimos y ejecutamos la consulta SQL
                 sql_query = f"DELETE FROM {table_name} WHERE {where_condition}"
                 values = tuple([id])
 
-                print(f"sql_query: {sql_query}, values: {values}")        
-
                 cursor.execute(sql_query, values)
-
                 connection.commit()
 
                 return {'type': 'success', 'message': 'Registro eliminado'}
@@ -203,29 +217,30 @@ def delete_record_table_by_id(token: str, id: str, connection: mysql.connector.c
     else:
         return {"type": "error", "message": "Vista de origen no existe"}
 
-
+# función para verificamos si un usuario está autenticado
 def user_is_authenticated(token: str, connection: mysql.connector.connection.MySQLConnection, secret_key: str) -> bool:
     decoded_token = jwt.decode(token, secret_key, algorithms='HS256')
     user_name = decoded_token["user_name"]
     user_id = decoded_token["user_id"]
     user_rol = decoded_token["user_rol"]
-    
-    try:        
+
+    try:
         cursor = connection.cursor(dictionary=True) 
         sql_query = "SELECT Id, Perfil FROM Usuarios WHERE Usuario = %s"
         cursor.execute(sql_query, (user_name,))
 
         record = cursor.fetchone()
-        
+
+        # verificamos si el usuario existe en la base de datos y si los datos coinciden
         if record and user_id == record['Id'] and user_rol == record['Perfil']:
             return True
-        
+
         return False
-        
+
     except Exception:
         return False
-    
 
+# función para autenticar a un usuario
 def auth_user(user_name: str, connection: mysql.connector.connection.MySQLConnection, password: str, secret_key: str) -> str:
     try:
         cursor = connection.cursor(dictionary=True)
@@ -234,24 +249,23 @@ def auth_user(user_name: str, connection: mysql.connector.connection.MySQLConnec
 
         record = cursor.fetchone()
 
+        # verificamos si el usuario existe y si la contraseña coincide
         if record and record['Contraseña'] == password:
             user_id = record['Id']
             user_rol = record['Perfil']
             print(f"Usaurio {user_name} autenticado exitosamente!")
 
+            # generamos un token de autenticación
             token = jwt.encode({'user_id': user_id, 'user_name': user_name, 'user_rol': user_rol}, secret_key, algorithm='HS256')
             return token
 
         else:
             print(f"Autenticacion fallida para el usuario {user_name}")
             return None
+
     except mysql.connector.Error as err:
         print(f"Error: {err}")
         return None
+
     finally:
         cursor.close()
-        
-
-
-
-    
